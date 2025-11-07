5ad42d472efbd969dcf8e74b3f173e9e6cddf10d
Omkar  Durge <durgeomkar86@gmail.com>
2025-11-07 23:27:10 +0530

    Add files via upload

---
+++ PPA.java
@@ -0,0 +1,18 @@
+import Marvellous.PHello;
+import Marvellous.PDemo;
+import Marvellous.Infosystem.LB;
+
+class PPA {
+
+    public static void main(String A[]) {
+        PHello hobj = new PHello();
+        PDemo dobj = new PDemo();
+
+        LB lobj=new LB();
+
+        hobj.fun();
+        dobj.gun();
+        lobj.run();
+    }
+
+}
